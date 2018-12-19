406588cb4c13c2a14a98c8ed667722fe955357ec
Ranjeet Nityanand <ranjeetnityanand@gmail.com>
2018-12-19 15:24:59 +0530

    Spring Boot Commit

---
+++ src/main/java/com/boot/config/EmpManagementApplication.java
@@ -0,0 +1,14 @@
+package com.boot.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpManagementApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/boot/config/EmpManagementApplicationTests.java
@@ -0,0 +1,17 @@
+package com.boot.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmpManagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
