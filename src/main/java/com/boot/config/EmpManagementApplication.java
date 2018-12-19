27d254545cfc4e2dee04b849ddf8224b98abdd19
Ranjeet Nityanand <ranjeetnityanand@gmail.com>
2018-12-19 18:00:21 +0530

    I have removed only space

--- src/main/java/com/boot/config/EmpManagementApplication.java
+++ src/main/java/com/boot/config/EmpManagementApplication.java
@@ -1,7 +1,5 @@
 package com.boot.config;
-
 import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
